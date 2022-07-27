6187020c7185d15c8608f872c50d99f8761cabd5
Jaydon <pearcejaydon@gmail.com>
2022-07-27 20:00:05 +1000

    Add logic to calculate user input and display result

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
+//this is used to change the display number and get the user input number
+const displayNumber = document.querySelector('.calc-display');
+
+//every time you click a number call updatedisplay which concatenates text to display number
 const numbers = Array.from(document.querySelectorAll(".numbers"));
 numbers.forEach(number => number.addEventListener('click', updateDisplay));
 
+//clears all saved variables to default
 const clearButton = document.querySelector('.clear');
-clearButton.addEventListener('click', clearCalc);
+clearButton.addEventListener('click', clearDisplay);
 
-const displayNumber = document.querySelector('.calc-display');
+//when selecting an operation, save selection and first number
+const operators = Array.from(document.querySelectorAll('.operators'));
+operators.forEach(operation => operation.addEventListener('click', selectOperator));
 
+//when click equals saves second number and runs operate
+const equalsButton = document.querySelector('.equals');
+equalsButton.addEventListener('click', selectEquals);
 
-function clearCalc(){
+//vars that get changed depending on user inputs
+let firstNumber = 0;
+let secondNumber = 0;
+let selectedOperation = "";
+
+function selectEquals(){
+    //save the users input as a float value to be used in calculations
+    secondNumber = parseFloat(displayNumber.textContent);
+    //update display to the output of user selected operation
+    displayNumber.textContent = operate(selectedOperation, firstNumber, secondNumber);
+}
+
+function selectOperator(){
+    firstNumber = parseFloat(displayNumber.textContent);
+    selectedOperation = this.classList[1];
+    displayNumber.textContent = 0;
+}
+
+function clearDisplay(){
+    firstNumber = 0;
+    secondNumber = 0;
+    selectedOperation = "";
     displayNumber.textContent = 0;
 }
 
@@ -17,6 +48,7 @@ function updateDisplay(e){
         displayNumber.textContent = "";
     }
 
+    //concatenate the text from the button user clicked on
     displayNumber.textContent = displayNumber.textContent + this.textContent;
 }
 
